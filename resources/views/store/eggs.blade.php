e3f1f4f421e1fd703263237cddb636a3fc2220d2
heysarahshere <heysarahshere@gmail.com>
2022-07-25 12:40:42 -0700

    Egg page changes

--- resources/views/store/eggs.blade.php
+++ resources/views/store/eggs.blade.php
@@ -5,17 +5,20 @@
 @section('content')
 
     <div>
-        <div class="container pt-5 store-body">
+        <div class="container pt-5 store-body text-center">
+            <h1>Creature Eggs</h1>
+            <h2>Ready to hatch!</h2>
+            <hr>
             <div class="row justify-content-center align-items-center m-auto">
                 @foreach($eggs as $egg)
                     <div class="col-3">
                         <div class="card {{ $egg->element }}" style="width: 100%;">
                             <img class="card-img-top" src="{{ Storage::disk('s3')->url($egg->image) }}"
                                  alt="Card image cap">
-                            <div class="card-body">
+                            <div class="card-body py-0">
                                 <h2 class="card-title">{{ $egg->name }}</h2>
                                 <p class="card-text">{{ $egg->description }}</p>
-                                <a href="#" class="btn btn-primary ombre-btn mt-3">Purchase</a>
+                                <a href="#" class="btn btn-primary ombre-btn my-3">Purchase</a>
                             </div>
                         </div>
                     </div>
---
+++ resources/views/store/foods.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.master')
+@section('title')
+    Food Stuffs
+@endsection
+@section('content')
+
+
+
+@endsection
