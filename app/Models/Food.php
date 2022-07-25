a2e5b469a8800956c3f3fb4d083989fdf575ad36
imisplacedmyarm <109809618+imisplacedmyarm@users.noreply.github.com>
2022-07-25 16:22:36 -0700

    add food thing

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Food extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'description',
+        'magic',
+        'defense',
+    ];
 }
